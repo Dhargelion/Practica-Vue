5ab219029992f1aea7e40b2fe063423fa742fca4
DiegoCV <corralesvdiego@gmail.com>
2020-10-19 20:08:18 -0400

    estilos login

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <Login />
   </div>
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { Options, Vue } from "vue-class-component";
+import Login from "../components/Login.vue";
 
 @Options({
   components: {
-    HelloWorld,
+    Login,
   },
 })
 export default class Home extends Vue {}
